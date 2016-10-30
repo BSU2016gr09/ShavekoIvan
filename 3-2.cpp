db4008433ceae329caadbcb78500b3dffc7ee534
Shaveko_Ivan <shaveko.ivan@gmail.com>
2016-10-30 19:16:43 +0300

    homework

---
+++ 3-2.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include <locale>
+using namespace std;
+//В массиве размера N, заполненного случ.числами от 0 до 10, подсчитать количество элементов, встречающихся более одного раза.
+void createArray(int arr[], int N)
+{
+	int i = 0;
+	while (i < N)
+	{
+		arr[i] = rand() % 11;
+		i++;
+	}
+}
+void printArray(int arr[], int N)
+{
+	int i = 0;
+	while (i < N)
+	{
+		cout << arr[i] << ' ';
+		i++;
+	}
+	cout << endl;
+}
+void sortArray(int arr[], int N)
+{
+	int i = 0;
+	while (i < N-1)
+	{
+		int j = 0;
+		while (j < N - i - 1)
+		{
+			if (arr[j] > arr[j + 1]) swap(arr[j], arr[j + 1]);
+			j++;
+		}
+		i++;
+	}
+}
+void checkSame(int arr[], int N)
+{
+	int i = 0;
+	while (i < N)
+	{
+		int k = 1, j = i + 1;
+		while (j < N)
+		{
+			if (arr[i] == arr[j]) k++;
+			j++;
+		}
+		if (k > 1) cout << "Элемент " << arr[i] << " повторяется " << k << " раза" << endl;
+		i = i + k;
+	}
+}
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	const int N = 10;
+	int a[N];
+	createArray(a, N);
+	printArray(a, N);
+	sortArray(a, N);
+	printArray(a, N);
+	checkSame(a, N);
+	system("pause");
+}
---
+++ 3-3.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <locale>
+using namespace std;
+//В массиве размера N, заполненного случ.числами от 0 до 10, определить максимальную длину последовательности равных элементов.
+void createArray(int arr[], int N)
+{
+	int i = 0;
+	while (i < N)
+	{
+		arr[i] = rand() % 11;
+		i++;
+	}
+}
+void printArray(int arr[], int N)
+{
+	int i = 0;
+	while (i < N)
+	{
+		cout << arr[i] << ' ';
+		i++;
+	}
+	cout << endl;
+}
+int checkLong(int arr[], int N)
+{
+	int i = 0, max=0;
+	while (i < N)
+	{
+		int k = 1, j = i+1;
+		while (j < N)
+		{
+			if (arr[i] == arr[j]) k++;
+			else
+				if (k > max) max = k;
+			j++;
+		}
+		i = i + k;
+	}
+	return max;
+}
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int const N = 10;
+	int a[N];
+	createArray(a, N);
+	printArray(a, N);
+	cout <<"Максимальную длину последовательность равных элементов равна " << checkLong(a, N);
+	system("pause");
+}
