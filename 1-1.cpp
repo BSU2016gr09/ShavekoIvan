c8c20b5e6864b4027cafbc76268791c195ec2b33
Ivan <shaveko.ivan@gmail.com>
2017-02-22 21:21:00 +0300

    homework

---
+++ 1-1.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <locale>
+using namespace std;
+
+//Расположить в порядке возрастания элементы массива А(N), начиная с k-го элемента.
+
+void inputArray(int* arr, int N) {
+	for (int i = 0; i < N; i++) cin >> *(arr + i);
+}
+
+void printArray(int* arr, int N) {
+	for (int i = 0; i < N; i++) cout << *(arr + i) << " ";
+}
+
+void sortArray(int* arr, int N) {
+	int k;
+	cout << "Введите число, с которого начинаем сортировку\n";
+	cin >> k;
+	for (; k < N; k++)
+		for (int j=k; j < N; j++)
+			if (*(arr + k) > *(arr + j)) swap(*(arr + k),*(arr + j));
+}
+
+int main() {
+	setlocale(LC_ALL,"Russian");
+	int N;
+	cin >> N;
+	int* arr = new int[N];
+	cout << "Введите массив\n";
+	inputArray(arr, N);
+	sortArray(arr, N);
+	printArray(arr, N);
+	delete[] arr;
+	arr = nullptr;
+	system("pause");
+}
---
+++ 1-2.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <locale>
+#include <math.h>
+using namespace std;
+
+/*Даны точки плоскости своими координатами в виде двух одномерных массивов.
+Точки плоскости рассортировать по возрастанию расстояния до прямой ax + by + c = 0.*/
+
+void inputArray(int* x, int* y, int N) {
+	for (int i = 0; i < N; i++)
+	{
+		cin >> *(x + i);
+		cin >> *(y + i);
+	}
+}
+
+void printArray(int* x, int* y, int N) {
+	for (int i = 0; i < N; i++) cout << "(" << x << ";" << y << ") ";
+}
+
+void countDistance(int* x, int* y, int* distance, int a, int b, int c, int N) {
+	for (int i = 0; i < N; i++) *(distance + i) = abs(a * x[i] + b * y[i] + c) / sqrt(a * a + b * b);
+}
+
+void sortDistance(int* distance, int* x, int* y, int N) {
+	for (int i=0; i < N; i++)
+		for (int j = 0; j < N; j++)
+			if (*(distance + i) > *(distance + j))
+			{
+				swap(*(distance + i), *(distance + j));
+				swap(*(x + i), *(x + j));
+				swap(*(y + i), *(y + j));
+			}
+}
+
+int main() {
+	int N, a, b, c;
+	cin >> N;
+	int* x = new int[N];
+	int* y = new int[N];
+	int* distance = new int[N];
+	cout << "Введите коффициенты a, b, c уравнения прямой\n";
+	cin >> a;
+	cin >> b;
+	cin >> c;
+	inputArray(x, y, N);
+	countDistance(x, y, distance, a, b, c, N);
+	sortDistance(distance, x, y, N);
+	printArray(x, y, N);
+	delete[] x;
+	x = nullptr;
+	delete[] y;
+	y = nullptr;
+	delete[] distance;
+	distance = nullptr;
+	system("pause");
+}
