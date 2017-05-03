25a200855215a6bf15a8bc95c8c2a84250e68fd6
IvanShaveko <shaveko.ivan@gmail.com>
2017-05-03 23:14:49 +0300

    complex

--- complex.cpp
+++ complex.cpp
@@ -3,53 +3,116 @@
 
 using namespace std;
 
-class Complex{//неужели нечего в private секцию засунуть????
+class Complex{
 public:
-    double re;
-    double im;
+    Complex(){//конструктор по-умолчанию
+        this->re=0;
+        this->im=0;
+        cout<<"Конструктор по-умолчанию\n";
+    }
 
-    void printComplex(){
-        cout<<re<<"+"<<im<<" *i"<<'\n';
+    Complex(Complex&a){//конструктор копирование
+        this->re=a.re;
+        this->im=a.im;
+        cout<<"Конструктор копирования для" <<(this->re)<<(*this).im<<"\n";
+    }
+
+    Complex(double b, double c){
+        this->re=b;
+        this->im=c;
+        cout<<"Конструктор с параметром\n";
+    }
+
+    ~Complex(){
+        cout<<"Деструктор\n";
+    }
+
+    Complex& operator=(Complex& other){
+        this->re=other.re;
+        this->im=other.im;
+        cout<<"Оператор присваивания\n";
+        return *this;
+    }
+
+    Complex& operator=(const double& b){
+        this->re=b;
+        this->im=0;
+        cout<<"Оператор присваивания\n";
+        return *this;
+    }
+
+    void print(){
+        cout<<this->re<<"+"<<this->im<<"*i"<<'\n';
     }
 
     double abs(){//module
-        return sqrt(re*re+im*im);
+        return sqrt(this->re*this->re+this->im*this->im);
     }
 
-    Complex sum(Complex other){
-        Complex rez;
-        rez.re=this->re+other.re;
-        rez.im=this->im+other.im;
-        return rez;
+    Complex& operator+(Complex& other){
+        this->re+=other.re;
+        this->im+=other.im;
+        return *this;
     }
 
-    Complex sub(Complex other){
-        Complex rez;
-        rez.re=this->re-other.re;
-        rez.im=this->im-other.im;
-        return rez;
+    Complex& operator+(const double& b){
+        re+=b;
+        return *this;
     }
 
-    Complex multi(Complex other){
-        Complex rez;
-        rez.re=this->re*other.re-this->im*other.im;
-        rez.im=this->re*other.im+this->im*other.re;
-        return rez;
+    Complex& operator- (Complex& other){
+        this->re-=other.re;
+        this->im-=other.im;
+        return *this;
     }
+
+    Complex& operator*(Complex& other){
+        this->re=this->re*other.re-this->im*other.im;
+        this->im=this->re*other.im+this->im*other.re;
+        return *this;
+    }
+
+    void setRe(double a){
+        this->re=a;
+    }
+
+    void setIm(double a){
+        this->im=a;
+    }
+
+private:
+    double re;
+    double im;
 };
 
 int main(){
-    Complex a, b, c;
-    a.re=2;
-    a.im=3;
-    b.re=2;
-    b.im=3;
+    const int N=10;
+
+    Complex a(5, 3), b(2,3), c(a), k;
+    Complex *pn, *pm;
+    pn=&b;
+    pm=new Complex(0, 1);//объявили мнимую 1
+    Complex A(5, 0);//объявили 5
+    Complex arr[N];
+    double tmp;
+    cout<<"Введите действительную часть комплексного числа\n";
+    cin>>tmp;
+    k.setRe(tmp);
+    cout<<"Введите мнимую часть комплексного числа\n";
+    cin>>tmp;
+    k.setIm(tmp);
+    k.print();
+    for(int i=0; i<N;i++) {Complex tmp(rand(), rand());arr[i]=tmp;}
+    for(int i=0; i<N;i++) {arr[i].print();}
+    double g=0;
+    k=g;
+    k=15;
     cout<<a.abs()<<'\n';
-    c=a.sum(b);
-    c.printComplex();
-    c=a.sub(b);
-    c.printComplex();
-    c=a.multi(b);
-    c.printComplex();
+    a=b-c;
+    a.print();
+    k=b+c;
+    k.print();
+    k=b*b;
+    k.print();
     return 0;
 }
